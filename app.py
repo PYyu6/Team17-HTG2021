087fc809a4fc0cde3b834191ee244b9b767da003
Joshua Sheng <jsdragonok2@gmail.com>
2021-03-14 08:51:24 -0400

    added more get features, sanitized decimal output

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def sanitize_decimal(dtuple):
 def respond():
     req = request.get_json(silent=True, force=True)
     query_result = req.get('queryResult')
+    output_params = query_result.get("outputContexts").get("parameters")
     params = query_result.get("parameters")
     return {
       "fulfillmentMessages": [
