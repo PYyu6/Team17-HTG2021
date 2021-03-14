66836569d43e024da7fb88b39bb1d2d7d8706547
Joshua Sheng <jsdragonok2@gmail.com>
2021-03-14 09:23:36 -0400

    added more get features, sanitized decimal output

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def respond():
         {
           "text": {
             "text": [
-              lastcontext.get("sheltername")
+              response
             ]
           }
         }
@@ -104,24 +104,29 @@ def post_rating(institution_raw, initiative_raw, rating):
         initiative = cursor.fetchone()[0]
     return "$"
     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-#     if rating and (institution or initiative):
-#         if rating.isdigit() and int(rating) < 5:
-#             if initiative:
-#                 query = "INSERT INTO ratings (institution_id, rating) VALUES (%s, %s)" %(institution, rating)
-#             else:
-#                 query = "INSERT INTO ratings (initiative_id, rating) VALUES (%s, %s)" %(initiative, rating)
-#         else:
-#             if initiative:
-#                 query = "INSERT INTO ratings (institution_id, review) VALUES (%s, '%s')" %(institution, rating)
-#             else:
-#                 query = "INSERT INTO ratings (initiative_id, review) VALUES (%s, '%s')" %(initiative, rating)
-#              
-#         print(query)
-#         cursor.execute(query)
-#         conn.commit();
-#         return "Thanks for leaving a review"
-#     else:
-#         return "Sorry, this institution or initiative does not exist in our system"
+    if rating and (institution or initiative):
+        if rating.isdigit() and int(rating) < 5:
+            if initiative:
+                query = "INSERT INTO ratings (institution_id, rating) VALUES (%s, %s)" %(institution, rating)
+            else:
+                query = "INSERT INTO ratings (initiative_id, rating) VALUES (%s, %s)" %(initiative, rating)
+        else:
+            if initiative:
+                query = "INSERT INTO ratings (institution_id, review) VALUES (%s, '%s')" %(institution, rating)
+            else:
+                query = "INSERT INTO ratings (initiative_id, review) VALUES (%s, '%s')" %(initiative, rating)
+              
+        print(query)
+        cursor.execute(query)
+        conn.commit();
+        cursor.close()
+        conn.close()
+        return "Thanks for leaving a review"
+    else:
+        cursor.close()
+        conn.close()
+        return "Sorry, this institution or initiative does not exist in our system"
+
 
 # A welcome message to test our server
 @app.route('/')
